dfb1304f9fce55368c558ea6faba0e34d797a4c4
KatherinChavez <kchavezb@ucenfotec.ac.cr>
2022-03-12 12:44:45 -0600

    Create JS main.js

---
+++ js/JS main.js
@@ -0,0 +1,3 @@
+// variable:es un espacio en memoria donde se almacena información
+// declaracion es un cuando se pone un valor a un espacio en memoria
+//asignacion cuando se le asigna un  valor a un espacio en memoria
